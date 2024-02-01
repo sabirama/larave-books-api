1d6d58c1b3879aa02fc972b193fe1d176cfcce6f
sabirama <markcondes21@gmail.com>
2024-02-01 15:24:27 +0800

    fixed framework

---
+++ storage/framework/views/d932ec25394fbd9e3fb3b4fa451003d3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <style>
+        body {
+            height: 95svh;
+            width: 99svw;
+        }
+
+        div {
+            height: 100%;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            align-items:center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <h1>SERVER RUNNING...</h1>
+    </div>
+</body>
+</html>
+<?php /**PATH D:\Projects\Web_Development\onDevelopment\Php\laravel-project\resources\views/welcome.blade.php ENDPATH**/ ?>
